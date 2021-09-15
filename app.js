0839226742f5e981d6de7d2ce987bbdded08e679
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-09-16 03:09:31 +0530

    logic part done

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const billAmount = document.querySelector("#bill-amount");
 const checkbutton = document.querySelector("#check-button");
 const cashGiven = document.querySelector("#cash-given");
 const message = document.querySelector("#error-message");
-// console.log(billAmount.value);
+const noOfNotes = document.querySelector(".no-of-notes");
+const availablenotes = [2000,500,100,20,10,5,1];
 checkbutton.addEventListener("click", function validbillamount() {
-  message.style.display = "none";
+  hideMessage();
 
   if (billAmount.value > 0) {
     if (cashGiven.value >= billAmount.value) {
+      const amountToBeReturned = cashGiven.value - billAmount.value;
+      calculateChange(amountToBeReturned);
     } else {
       showMessage(
         "cash given amount should be grater and or equal to bill amount"
@@ -17,8 +20,19 @@ checkbutton.addEventListener("click", function validbillamount() {
     showMessage("please enter bill amount greated than 0");
   }
 });
+function hideMessage (){
+  message.style.display = "none";
+}
 
-function calculateChange() {}
+function calculateChange() {
+  for(var i=0; i < availablenotes.length; i++){
+    const numberOfNotes = Math.trunc(
+      amountToBeReturned / availablenotes[i]
+    )
+    amountToBeReturned %= availablenotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
 
 function showMessage(text) {
   message.style.display = "block";
