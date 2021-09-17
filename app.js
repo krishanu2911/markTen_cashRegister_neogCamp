777d9cef70e5e1f3768050710ba1142c104ba95d
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-09-17 23:23:41 +0530

    css is updated

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const checkbutton = document.querySelector("#check-button");
 const cashGiven = document.querySelector("#cash-given");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
-console.log(noOfNotes);
+
 const availablenotes = [2000, 500, 100, 20, 10, 5, 1];
 checkbutton.addEventListener("click", function validbillamount() {
   hideMessage();
 
-  if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
+  if ( Number( billAmount.value) > 0) {
+    if (Number(cashGiven.value) >= Number(billAmount.value)) {
+      const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
       calculateChange(amountToBeReturned);
     } else {
       showMessage(
